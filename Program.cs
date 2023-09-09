1c1f40c0f2ae40f69ca3d3762aa125ddd389a7a9
Yulissa20220818 <hyulissa830@gmail.com>
2023-09-08 20:00:10 -0600

    Clase09_U20220818

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("\n----------PROGRAMACION DE CALIFICACIONES--------\n");
+            Console.WriteLine("Ingresar la calificacion: ");
+            double calificacion = Convert.ToDouble(Console.ReadLine());
+
+            if (calificacion >= 10.0){
+                Console.WriteLine("La nota "+ "es Excelente");
+            }
+            else if (calificacion > 8.0){
+                Console.WriteLine("La nota "+ "es Muy Buena");
+            }
+            else if (calificacion >7.5){
+                Console.WriteLine("La nota "+ "es Buena");
+            }
+            else{
+                Console.Write("La nota obtenida de Deficiente");
+            }
+            Console.WriteLine("\n----FIN DE PROGRAMACION DE CALIFICACIONES-----\n");
+        }
+    }
+}
